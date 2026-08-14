2ceb7ae5b991d3d551d48d2b299905aaab3441e4
rm-netsu <netsu.code@gmail.com>
2026-08-14 22:42:39 +0000

    test: execute the complete protocol suite

--- src/root.zig
+++ src/root.zig
@@ -7,7 +7,21 @@ pub const http1 = @import("http1.zig");
 pub const http2 = @import("http2.zig");
 
 test {
-    _ = common;
-    _ = http1;
-    _ = http2;
+    _ = common.Header;
+    _ = common.isFieldValue;
+
+    _ = http1.head.HeadParser;
+    _ = http1.body.ChunkDecoder;
+    _ = http1.write.requestHead;
+
+    _ = http2.frame.FrameDecoder;
+    _ = http2.settings.Settings;
+    _ = http2.flow.FlowWindow;
+    _ = http2.fields.Validator;
+    _ = http2.payload.HeadersPayload;
+    _ = http2.continuation.Guard;
+    _ = http2.preface.Parser;
+    _ = http2.protocol.ErrorCode;
+    _ = http2.stream.Stream;
+    _ = http2.header_block.Collector;
 }
