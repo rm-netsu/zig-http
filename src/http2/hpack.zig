3794b379eba4e9905b9858de6cbd7fcad9693360
rm-netsu <netsu.code@gmail.com>
2026-08-14 16:11:30 +0000

    feat: implement HPACK Huffman coding

--- src/http2/hpack.zig
+++ src/http2/hpack.zig
@@ -1,7 +1,9 @@
+pub const huffman = @import("hpack/huffman.zig");
 pub const static = @import("hpack/static.zig");
 pub const table = @import("hpack/table.zig");
 
 test {
+    _ = huffman;
     _ = static;
     _ = table;
 }
