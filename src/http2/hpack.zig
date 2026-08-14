a5dc81f89808dd089b5d6e547776af05ceed7adc
rm-netsu <netsu.code@gmail.com>
2026-08-14 16:12:00 +0000

    feat: add HPACK streaming encoder and decoder

--- src/http2/hpack.zig
+++ src/http2/hpack.zig
@@ -1,9 +1,16 @@
 pub const huffman = @import("hpack/huffman.zig");
 pub const static = @import("hpack/static.zig");
 pub const table = @import("hpack/table.zig");
+pub const codec = @import("hpack/codec.zig");
+
+pub const Decoder = codec.Decoder;
+pub const Encoder = codec.Encoder;
+pub const Indexing = codec.Indexing;
+pub const HuffmanMode = codec.HuffmanMode;
 
 test {
     _ = huffman;
     _ = static;
     _ = table;
+    _ = codec;
 }
